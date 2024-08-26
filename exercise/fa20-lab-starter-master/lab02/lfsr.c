49e43a6068185ede321301f8a275f302cdd47310
1nram1 <1229595995@qq.com>
2024-08-26 15:59:20 +0800

    finished!

--- exercise/fa20-lab-starter-master/lab02/lfsr.c
+++ exercise/fa20-lab-starter-master/lab02/lfsr.c
@@ -4,7 +4,42 @@
 #include <string.h>
 #include "lfsr.h"
 
+
+// Return the nth bit of x.
+// Assume 0 <= n <= 31
+unsigned get_bit(unsigned x,
+                 unsigned n) {
+    // YOUR CODE HERE
+    // Returning -1 is a placeholder (it makes
+    // no sense, because get_bit only returns 
+    // 0 or 1)
+    return (x >> n) & 1;
+}
+// Set the nth bit of the value of x to v.
+// Assume 0 <= n <= 31, and v is 0 or 1
+void set_bit(unsigned * x,
+             unsigned n,
+             unsigned v) {
+    // YOUR CODE HERE
+    if(v == 1) {
+        *x |= (1 << n);
+    } else {
+        *x &= ~(1 << n);
+    }
+
+}
+
+
+void flip_bit(unsigned * x,
+              unsigned n) {
+    // YOUR CODE HERE
+    *x ^= (1 << n);
+}
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned MSB = get_bit(*reg, 0) ^ get_bit(*reg, 2) ^ get_bit(*reg, 3) ^ get_bit(*reg, 5);
+    *reg = *reg >> 1;
+    set_bit(reg, 15, MSB);
 }
 
